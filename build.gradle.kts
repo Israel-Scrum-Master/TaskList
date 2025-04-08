5ba2cb99c49fd2905d1e54b414cadf3d1c87c172
Israel <issrael808@gmail.com>
2025-04-07 22:31:09 -0600

    Primer commit del proyecto TaskList

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+// build.gradle.kts (Proyecto)
 plugins {
+    // Plugins de nivel superior
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
+}
+
+subprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
